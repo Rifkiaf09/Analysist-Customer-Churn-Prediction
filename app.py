29a76a6b5668b3053755224f24f8524882e2b0db
Rifki Ahmad <68650364+Rifkiaf09@users.noreply.github.com>
2025-06-01 09:36:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,35 @@
-
 import streamlit as st
-import pickle
+import joblib
 import pandas as pd
 
-# Load the trained model
-model = pickle.load(open("pipeline_xgb.pkl", "rb"))
+# Load model
+model = joblib.load('model_pipeline.pkl')
 
-# Define input fields
-st.title("Telco Customer Churn Prediction")
+# Judul
+st.title("Customer Churn Prediction")
 
-gender = st.selectbox("Gender", ["Male", "Female"])
-SeniorCitizen = st.selectbox("Senior Citizen", [0, 1])
-Partner = st.selectbox("Has Partner", ["Yes", "No"])
-Dependents = st.selectbox("Has Dependents", ["Yes", "No"])
-tenure = st.slider("Tenure (months)", 0, 72, 12)
-PhoneService = st.selectbox("Phone Service", ["Yes", "No"])
-MultipleLines = st.selectbox("Multiple Lines", ["Yes", "No", "No phone service"])
-InternetService = st.selectbox("Internet Service", ["DSL", "Fiber optic", "No"])
-OnlineSecurity = st.selectbox("Online Security", ["Yes", "No", "No internet service"])
-OnlineBackup = st.selectbox("Online Backup", ["Yes", "No", "No internet service"])
-DeviceProtection = st.selectbox("Device Protection", ["Yes", "No", "No internet service"])
-TechSupport = st.selectbox("Tech Support", ["Yes", "No", "No internet service"])
-StreamingTV = st.selectbox("Streaming TV", ["Yes", "No", "No internet service"])
-StreamingMovies = st.selectbox("Streaming Movies", ["Yes", "No", "No internet service"])
-Contract = st.selectbox("Contract", ["Month-to-month", "One year", "Two year"])
-PaperlessBilling = st.selectbox("Paperless Billing", ["Yes", "No"])
-PaymentMethod = st.selectbox("Payment Method", ["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"])
-MonthlyCharges = st.number_input("Monthly Charges", min_value=0.0, value=70.0)
-TotalCharges = st.number_input("Total Charges", min_value=0.0, value=2000.0)
+# Input user
+gender = st.selectbox('Gender', ['Male', 'Female'])
+SeniorCitizen = st.selectbox('Senior Citizen', [0, 1])
+Partner = st.selectbox('Partner', ['Yes', 'No'])
+Dependents = st.selectbox('Dependents', ['Yes', 'No'])
+tenure = st.slider('Tenure', 0, 72, 1)
+MonthlyCharges = st.number_input('Monthly Charges')
+TotalCharges = st.number_input('Total Charges')
 
-# Prediction
-if st.button("Prediksi Churn"):
-    input_df = pd.DataFrame([{
-        'gender': gender,
-        'SeniorCitizen': SeniorCitizen,
-        'Partner': Partner,
-        'Dependents': Dependents,
-        'tenure': tenure,
-        'PhoneService': PhoneService,
-        'MultipleLines': MultipleLines,
-        'InternetService': InternetService,
-        'OnlineSecurity': OnlineSecurity,
-        'OnlineBackup': OnlineBackup,
-        'DeviceProtection': DeviceProtection,
-        'TechSupport': TechSupport,
-        'StreamingTV': StreamingTV,
-        'StreamingMovies': StreamingMovies,
-        'Contract': Contract,
-        'PaperlessBilling': PaperlessBilling,
-        'PaymentMethod': PaymentMethod,
-        'MonthlyCharges': MonthlyCharges,
-        'TotalCharges': TotalCharges
-    }])
+# ... tambahkan semua input sesuai fitur dari dataset
 
+if st.button('Predict Churn'):
+    input_df = pd.DataFrame({
+        'gender': [gender],
+        'SeniorCitizen': [SeniorCitizen],
+        'Partner': [Partner],
+        'Dependents': [Dependents],
+        'tenure': [tenure],
+        'MonthlyCharges': [MonthlyCharges],
+        'TotalCharges': [TotalCharges],
+        # tambahkan kolom lainnya
+    })
+    
     prediction = model.predict(input_df)[0]
-    if prediction == 1:
-        st.error("Pelanggan berpotensi CHURN!")
-    else:
-        st.success("Pelanggan diprediksi TIDAK CHURN.")
+    st.write('Churn' if prediction else 'Not Churn')
